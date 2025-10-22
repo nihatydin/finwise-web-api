b4c4e2b26f11b48c749382288332c3e99e7da14f
Nihat Aydın <nihatydin1@gmail.com>
2025-10-22 13:50:26 +0300

    example controller created

---
+++ Controllers/ExampleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace finwise_web_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExampleController : ControllerBase
+    {
+        public IActionResult GetMessage()
+        {
+            return Ok("ExampleController çalışıyor!");
+        }
+    }
+}
